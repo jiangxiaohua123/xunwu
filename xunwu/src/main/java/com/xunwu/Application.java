006e956e5b0f8403b6cbf4eb8bfe24934262dc99
jiangxiaohua <363369419@qq.com>
2019-07-11 23:53:24 +0800

    增加SpringData JPA + Hiberite集成

--- xunwu/src/main/java/com/xunwu/Application.java
+++ xunwu/src/main/java/com/xunwu/Application.java
@@ -2,13 +2,22 @@ package com.xunwu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@RestController
 public class Application {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	} 
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello spring boot";
+	}
+	
 	
 }
